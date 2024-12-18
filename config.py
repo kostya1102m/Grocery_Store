fce9cca03837c51413794ea79aa1e2ba4be81fb6
mercury <konstantin1102m@mail.ru>
2024-12-19 03:16:00 +1000

    add secret_key field

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
+    secret_key: str
 
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
