116dfe0c95cd42431fcc022186f3b401a8eb01a9
mercury <konstantin1102m@mail.ru>
2024-12-28 23:56:26 +1000

    clear code

--- routers/token.py
+++ routers/token.py
@@ -1,21 +1,19 @@
-
 from fastapi import Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from loguru import logger
 from config import settings
 from routers.utils import get_db
 from models import User
-
 
 SECRET_KEY = settings.secret_key
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
@@ -27,6 +25,7 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+
 async def get_current_user(token: str = Depends(oauth2_scheme), session: AsyncSession = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=401,
@@ -34,12 +33,12 @@ async def get_current_user(token: str = Depends(oauth2_scheme), session: AsyncSe
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        logger.info(f"token : {token}")
+        # logger.info(f"token : {token}")
 
         payload = jwt.decode(token, settings.secret_key, algorithms=[ALGORITHM])
         phone: str = payload.get("user_phone")
 
-        logger.info(f"phone : {phone}")
+        # logger.info(f"phone : {phone}")
 
         if phone is None:
             raise credentials_exception
