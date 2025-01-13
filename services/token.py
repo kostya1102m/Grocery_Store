f05f370d5a528cc3b1a5b2b1a980b752f239d434
mercury <konstantin1102m@mail.ru>
2025-01-14 01:15:42 +1000

    add verify_manager_role func

--- services/token.py
+++ services/token.py
@@ -4,9 +4,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
+
+from starlette import status
+from starlette.requests import Request
+
 from config import settings
-from routers.utils import get_db
-from models import User
+from utils import get_db
+from models.tables import User
 
 SECRET_KEY = settings.secret_key
 ALGORITHM = "HS256"
@@ -28,24 +32,50 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
 
 async def get_current_user(token: str = Depends(oauth2_scheme), session: AsyncSession = Depends(get_db)):
     credentials_exception = HTTPException(
-        status_code=401,
+        status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        # logger.info(f"token : {token}")
-
         payload = jwt.decode(token, settings.secret_key, algorithms=[ALGORITHM])
         phone: str = payload.get("user_phone")
-
-        # logger.info(f"phone : {phone}")
 
         if phone is None:
             raise credentials_exception
     except JWTError:
         raise credentials_exception
+
     user = await session.execute(select(User).where(User.phone == phone))
     user = user.scalars().first()
+
     if user is None:
-        raise credentials_exception
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Пользователя с таким номером нет")
     return user
+
+
+async def get_current_role(request: Request):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials. You are not authorized",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        token = request.cookies.get("token")
+
+        if token is None:
+            raise credentials_exception
+
+        payload = jwt.decode(token, settings.secret_key, algorithms=[ALGORITHM])
+        role: str = payload.get("role")
+
+    except JWTError:
+        raise credentials_exception
+
+    return role
+
+async def verify_manager_role(role: str = Depends(get_current_role)):
+    if role != "manager":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Недостаточно прав для выполнения данного действия",
+        )
