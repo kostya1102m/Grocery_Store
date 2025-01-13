4bd16149f91631c0a23f0b814ad6b59fae0b0e9c
mercury <konstantin1102m@mail.ru>
2025-01-14 01:12:59 +1000

    add dependencies to routers(manager has access to majority of them), fix naming of routers

--- routers/product.py
+++ routers/product.py
@@ -1,28 +1,34 @@
 from fastapi import Depends, HTTPException, APIRouter
+from pydantic import ValidationError
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 from starlette.responses import JSONResponse
 
-from schemas import ProductCreate
-from models import Product
+from models.schemas import ProductCreate
+from models.tables import Product
+from services.token import verify_manager_role
 
-from routers.utils import create_item, get_items, update_product_quantity, delete_object, get_db
+from utils import create_item, get_items, update_product_quantity, delete_object, get_db
 
 router = APIRouter(
-    prefix="/product",
+    prefix="/products",
     tags=["Product"],
 )
 
 
-@router.post("/create")
+@router.post("/product/create", dependencies=[Depends(verify_manager_role)])
 async def create_product(product: ProductCreate, session: AsyncSession = Depends(get_db)):
     try:
         return await create_item(product, Product, session)
+    except ValidationError as e:
+        await session.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=e.errors())
     except IntegrityError:
         await session.rollback()
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail="Продукт с таким id уже существует.")
+                            detail="Ошибка сервера. Не удалось создать продукт.")
 
 
 @router.get("/")
@@ -30,18 +36,13 @@ async def get_products(session: AsyncSession = Depends(get_db)):
     return await get_items(Product, session)
 
 
-@router.put("/update")
+@router.put("/product/update/{product_id}", dependencies=[Depends(verify_manager_role)])
 async def update_product_quantity_put(flag: bool, product_id: int, quantity: int,
                                       session: AsyncSession = Depends(get_db)):
-    try:
-        return await update_product_quantity(flag, product_id, quantity, session)
-    except IntegrityError:
-        await session.rollback()
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail="Ошибка при обновлении продукта.")
+    return await update_product_quantity(flag, product_id, quantity, session)
 
 
-@router.delete("/remove/{product_id}")
+@router.delete("/product/remove_by_id/{product_id}", dependencies=[Depends(verify_manager_role)])
 async def delete_product(product_id: int, session: AsyncSession = Depends(get_db)):
     product = await delete_object(session, Product, product_id, 'id')
     report = {
