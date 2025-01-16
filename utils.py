007cb054ba4fcda99193a6cb56ba1a0bcdb3a499
mercury <konstantin1102m@mail.ru>
2025-01-17 01:58:40 +1000

    add customer and manager templates var, fix delete product function so it deletes product if it doesnt exists in any order

--- utils.py
+++ utils.py
@@ -5,15 +5,16 @@ from starlette import status
 from starlette.templating import Jinja2Templates
 
 from database import async_session_maker
-from models.tables import Product, User
+from models.tables import Product, User, Ordered_product
 
 
 async def get_db() -> AsyncSession:
     async with async_session_maker() as session:
         yield session
 
-
 templates = Jinja2Templates(directory="templates")
+customer_templates = Jinja2Templates(directory="templates/customer")
+manager_templates = Jinja2Templates(directory="templates/manager")
 
 
 async def create_item(item_create_model, item_model, session: AsyncSession = Depends(get_db)):
@@ -32,18 +33,30 @@ async def get_user_by_credentials(user_credentials, session: AsyncSession):
     result = await session.execute(query)
     return result.scalars().first()
 
-
 async def delete_object(session: AsyncSession, model, identifier, identifier_field):
-    result = await session.execute(select(model).where(getattr(model, identifier_field) == identifier))
-    obj = result.scalars().first()
+        result = await session.execute(select(model).where(getattr(model, identifier_field) == identifier))
+        obj = result.scalars().first()
 
-    if obj is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"{model.__name__} с таким идентификатором не существует.")
+        if obj is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"{model.__name__} с ключом {identifier} не найден")
 
-    await session.delete(obj)
-    await session.commit()
-    return obj
+        await session.delete(obj)
+        await session.commit()
+        return obj
+
+
+async def delete_product_by_id(session: AsyncSession, product_id):
+
+    search_prod_in_ordered_prods = await session.execute(
+        select(Ordered_product).where(Ordered_product.product_id == product_id))
+
+    orders = search_prod_in_ordered_prods.scalars().all()
+    if orders:
+        orders_id_massive = [order.order_id for order in orders]
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"Продукт с id {product_id} находится в заказах {orders_id_massive}. Удаление отказано.")
+
+    return await delete_object(session, Product, product_id, "id")
 
 
 async def get_items(item_model, session: AsyncSession = Depends(get_db)):
@@ -51,12 +64,13 @@ async def get_items(item_model, session: AsyncSession = Depends(get_db)):
     return items.scalars().all()
 
 
+
 async def update_product_quantity(flag, product_id, quantity, session: AsyncSession = Depends(get_db)):
     result = await session.execute(select(Product).where(Product.id == product_id))
     product = result.scalars().first()
 
     if product is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Продукт с таким id не существует.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Продукт с id {product_id} не существует.")
 
     if flag:
         product.quantity += quantity
