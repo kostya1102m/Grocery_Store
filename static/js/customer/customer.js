92916a68744bccfee2fcbb6e0644b1af4ed15490
mercury <konstantin1102m@mail.ru>
2025-01-17 01:46:59 +1000

    fix token expire, replace to js holder

--- static/js/customer/customer.js
+++ static/js/customer/customer.js
@@ -69,7 +69,7 @@ function setupNavigation() {
             e.preventDefault();
             window.location.href = '/';
             localStorage.removeItem('jwt_customer_token');
-            document.cookie = "token=; Max-Age=-1;";
+            document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
         }
     });
 }
